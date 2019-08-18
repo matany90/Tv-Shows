968bc7d1676aefbb076e6071d2f623e812523e8c
MATAN\matan <matany90@gmail.com>
2019-08-18 04:37:10 +0300

    Show screen WIP

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import CustomHeader from '../components/CustomHeader'
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import { View, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+import { Rating, AirbnbRating } from 'react-native-elements';
+import SCREEN_IMPORT from 'Dimensions'
+  
+const SCREEN_WIDTH = SCREEN_IMPORT.get('window').width
 
-export default ShowScreen = (props) => {
+const ShowScreen = (props) => {
     const show = props.navigation.getParam('show')
-        return (
-            <View>
-                <Text>{show.name}</Text>
-            </View>    
-        );  
-    };
+    console.log(show)
+    return (
+        <View style={{ width: SCREEN_WIDTH }}>
+            <Card style={{ flex: 0 }}>
+                <CardItem cardBody>
+                    <Image source={{ uri: show.image.original }}
+                        style={{ resizeMode: 'cover', height: 500, flex: 1 }} />
+                </CardItem>
+                <CardItem>
+                    <Body style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                        <Text note style={{ textAlign: 'center', alignSelf: 'center', marginBottom: 10 }}>
+                            {`Average rating: ${show.rating.average}`}
+                        </Text>
+                        <Rating type='star' startingValue={show.rating.average / 2} readonly imageSize={28} />
+                    </Body>
+                </CardItem>
+            </Card>
+        </View>
+    );
+};
+
+export default ShowScreen;    
 
