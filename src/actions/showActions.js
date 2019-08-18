1df2b27bb69bea60f96933ed2dc45381796295e3
MATAN\matan <matany90@gmail.com>
2019-08-18 15:55:17 +0300

    Refactor 1

--- src/actions/showActions.js
+++ src/actions/showActions.js
@@ -2,8 +2,8 @@ import { FETCH_SHOWS } from './types';
 import axios from 'axios';
 
 export const fetchShows = () => async dispatch => {
-    const shows = await axios.get('http://api.tvmaze.com/shows?page=0');
-
+    const shows = await axios.get('http://api.tvmaze.com/shows');
+    console.log(shows.data)
     dispatch({ type: FETCH_SHOWS, payload: shows.data });
 }
 
